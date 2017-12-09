bc7ccfd3dec788c3b74fa5248388e6585b403cea
Steven Song <ssong@e1z2r5p39.42.us.org>
2017-12-08 16:22:52 -0800

    yes

--- ft_lstadd.c
+++ ft_lstadd.c
@@ -6,13 +6,13 @@
 /*   By: ssong <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/08 14:50:14 by ssong             #+#    #+#             */
-/*   Updated: 2017/12/08 14:53:25 by ssong            ###   ########.fr       */
+/*   Updated: 2017/12/08 16:18:15 by ssong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstadd(t_list **alst, t_list *new)
+void	ft_lstadd(t_list **alst, t_list *new)
 {
 	new->next = *alst;
 	*alst = new;
--- ft_lstdel.c
+++ ft_lstdel.c
@@ -6,12 +6,13 @@
 /*   By: ssong <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/08 14:37:38 by ssong             #+#    #+#             */
-/*   Updated: 2017/12/08 14:49:34 by ssong            ###   ########.fr       */
+/*   Updated: 2017/12/08 16:18:30 by ssong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-void ft_lstdel(t_list **alst, void (*del)(void *, size_t))
+
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
 	t_list *tmp;
 
